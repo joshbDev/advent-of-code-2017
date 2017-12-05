c12519a430d823bb3673bf8625949f4717774a41
joshb <joshb@spotify.com>
2017-12-05 14:28:02 -0500

    feature(second-questions): add second questions for day 4

--- day4/run-code.js
+++ day4/run-code.js
@@ -2,19 +2,32 @@ const fs = require('fs');
 
 const input = fs.readFileSync('./input', 'utf8');
 const inputArray = input.split('\n');
-console.log(inputArray.length)
+let anagramNumber = Number(inputArray.length);
 const answer = inputArray.reduce((prev, curr) => {
   const passPhraseArray = curr.split(' ');
   let hasDupe = false;
+  let hasAnagram = false;
   passPhraseArray.forEach((item, index) => {
     const restOfArray = passPhraseArray.slice(index + 1);
     if (restOfArray.includes(item)) {
       hasDupe = true;
     }
+    restOfArray.forEach((compareItem) => {
+      if (compareItem.length !== item.length) {
+        return;
+      }
+      if (compareItem.split('').sort().join('') === item.split('').sort().join('')) {
+        hasAnagram = true;
+      }
+    });
   });
   if (hasDupe) {
     prev--;
   }
+  if (hasAnagram) {
+    anagramNumber--;
+  }
   return prev;
 }, inputArray.length);
-console.log(answer);
+console.log('DUPLICATE ANSWER', answer);
+console.log('ANAGRAM ANSWER', anagramNumber);
