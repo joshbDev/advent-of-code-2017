1ddd87ad699b5bcf9f6a5743b8e048588bb7e7a1
joshb <joshb@spotify.com>
2017-12-11 10:29:40 -0500

    feature(day11): update answer for day 11 pt2

--- day11/run-code.js
+++ day11/run-code.js
@@ -5,6 +5,7 @@ const input = fs.readFileSync('./input').toString('utf-8').trim();
 const directionSplit = input.split(',');
 
 const oppositDirections = [['s', 'n'], ['se', 'nw'], ['sw', 'ne']];
+let farthestAway = 0;
 
 const total = directionSplit.reduce((prev, curr) => {
  if(typeof prev[curr] === 'undefined') {
@@ -12,32 +13,44 @@ const total = directionSplit.reduce((prev, curr) => {
  } else {
    prev[curr]++
  }
+ const howFarAway = calculateHowFarAway(createRubric(oppositDirections, prev));
+ if (howFarAway > farthestAway) {
+   farthestAway = howFarAway;
+ }
  return prev;
 }, {});
+const final = calculateHowFarAway(createRubric(oppositDirections, total));
 
-const minusedTotal = oppositDirections.reduce((prev, curr) => {
-  const [one, two] = curr;
-  if (total[one] >= total[two]) {
-    prev[one] = total[one] - total[two];
+console.log('PT 1!', final);
+console.log('PT 2', farthestAway);
+
+function createRubric(configArr, totalObj) {
+  return configArr.reduce((prev, curr) => {
+    const [one, two] = curr;
+    const oneNumber = totalObj[one] || 0;
+    const twoNumber = totalObj[two] || 0;
+    if (oneNumber >= twoNumber) {
+      prev[one] = oneNumber - twoNumber;
+      return prev;
+    }
+    prev[two] = twoNumber - oneNumber;
     return prev;
-  }
-  prev[two] = total[two] - total[one];
-  return prev;
-}, {});
+  }, {});
+}
 
-let otherNumber = 0
-const finalSteps = Object.keys(minusedTotal).reduce((prev, curr) => {
-  if (curr.length === 1) { //this is either straight north or south and can be added regardless
-    prev += minusedTotal[curr];
+function calculateHowFarAway(obj) {
+  let otherNumber = 0;
+  return Object.keys(obj).reduce((prev, curr) => {
+    if (curr.length === 1) { //this is either straight north or south and can be added regardless
+      prev += obj[curr];
+      return prev;
+    }
+    if (!otherNumber) {
+      otherNumber = obj[curr];
+      prev += otherNumber;
+    } else {
+      prev += obj[curr] - otherNumber;
+    }
     return prev;
-  }
-  if (!otherNumber) {
-    otherNumber = minusedTotal[curr];
-    prev += otherNumber;
-  } else {
-    prev += minusedTotal[curr] - otherNumber;
-  }
-  return prev;
-}, 0);
-
-console.log(finalSteps);
+  }, 0);
+}
